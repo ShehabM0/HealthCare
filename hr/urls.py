0fa4cdbce2009de88892c4b84b786158b54b0296
ShehabM0 <shehabmohamed2357@gmail.com>
2024-03-10 09:02:03 +0200

    add pagination and GetProfile func

--- hr/urls.py
+++ hr/urls.py
@@ -6,6 +6,7 @@ app_name = 'hr'
 urlpatterns = [
     path('create-user/', views.CreateUserView, name = 'create-user'),
     path('list/', views.ListEmployeesView, name = 'list-employees'),
-    path('list/<str:type>/', views.ListEmployeesView, name = 'list-employees-type'),
+    path('list/<str:type>/', views.ListEmployeesView, name = 'list-employees-by-type'),
+    path('profile/<int:pk>/', views.GetProfile, name = 'get-employee-by-id'),
 ]
 
