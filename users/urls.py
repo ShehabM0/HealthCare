bad15052ca7a233cef0dd2ca50624fffc2417107
ShehabM0 <shehabmohamed2357@gmail.com>
2024-03-22 00:45:07 +0200

    edit url description

--- users/urls.py
+++ users/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('list/<str:type>/', views.ListUsers, name = 'list-users-by-type'),
 
     path('getUser/<int:pk>/', views.GetUser, name = 'get-user-by-id'),
-    path('update/<int:pk>/', views.UpdateUser, name = 'update-current-logged-in-user-profile'),     #TODO Authorization
-    path('delete/<int:pk>/', views.DeleteUser, name = 'delete-current-logged-in-user'),     #TODO Authorization
+    path('update/<int:pk>/', views.UpdateUser, name = 'update-user-profile'),     #TODO Authorization
+    path('delete/<int:pk>/', views.DeleteUser, name = 'delete-user'),     #TODO Authorization
 
     path('getCurrentUser/', views.GetCurrentUser, name = 'get-current-logged-in-user'),
-    path('update/', views.UpdateUser, name = 'update-current-logged-in-user-profile'),
+    path('update/', views.UpdateUser, name = 'update-currentuser-profile'),
     path('delete/', views.DeleteUser, name = 'delete-current-logged-in-user'),
 ]
 
