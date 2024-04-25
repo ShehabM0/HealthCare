4ba988eaa60d788d07ae71b8871fb4b00009bd16
ShehabM0 <shehabmohamed2357@gmail.com>
2024-04-25 12:22:12 +0200

    add chat app_name

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = 'chat'
+
 urlpatterns = [
     path('list-room-messages/', ListRoomMessagesView, name='list-room-messages'),
     path('list-my-rooms/', ListRoomsView, name='list-all-user-rooms'),
