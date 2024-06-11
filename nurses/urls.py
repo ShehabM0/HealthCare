d763945605c85c1889531d1043fcd7a79116e48d
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-06-11 03:06:49 +0300

    edite link

--- nurses/urls.py
+++ nurses/urls.py
@@ -5,17 +5,18 @@ from rest_framework import permissions
 app_name = 'nurses'
 
 urlpatterns = [   
-    path('/UserId/Get/', views.GetUserId.as_view(), name='GetUserId'),
+    path('UserId/Get/', views.GetUserId.as_view(), name='GetUserId'),
     path('profile/', views.GetProfile.as_view(), name='profile'),
     path('Casses/', views.GetPatientsClinic.as_view(), name='Casses'),
     path('Casses/Details/<int:id>', views.UserDetails.as_view(), name='UserDetails'),
+
     path('Rooms/<int:room_id>', views.GetCurrentRoom.as_view(), name='Room'),
- 
     path('Rooms/GetAll/', views.GetAllRooms.as_view(), name='AllRooms'),
     path('Rooms/History/<int:room_id>', views.GetRoomsHistory.as_view(), name='RoomsHistory'),
     path('Room/Booked/<int:room_id>', views.GetBookedRoom.as_view(), name='BookedRoom'),
     path('Room/Update/<int:bed_id>', views.UpdateRoom.as_view(), name='UpdateRoom'),
     path('Room/Add/<int:bed_id>', views.AddRoom.as_view(), name='AddRoom'),
+    
     path('Calls/Current/', views.GetCurrentCalls.as_view(), name='CurrentCalls'),
     path('Calls/<int:Call_id>', views.GetCalls.as_view(), name='Calls'),
     path('Calls/GetAll/', views.GetAllCalls.as_view(), name='AllCalls'),
