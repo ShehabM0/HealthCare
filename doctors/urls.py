35c2a17b536b99e47a7e4ad687efdb6cb0bf7dfe
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-06-14 05:51:47 +0300

    add link

--- doctors/urls.py
+++ doctors/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('patient/<int:id>', UserDetails.as_view()),
     path('cases/', DoctorCasesView.as_view()),
     path('records/<int:patient_id>' , PatientMedicalRecordView.as_view()),
+    
     path('Rooms/<int:room_id>', GetCurrentRoom.as_view(), name='Room'),
     # path('Bed/', GetBed.as_view(), name='Bed'),
     path('Rooms/GetAll/', GetAllRooms.as_view(), name='AllRooms'),
@@ -24,9 +25,12 @@ urlpatterns = [
     path('Room/Booked/<int:room_id>', GetBookedRoom.as_view(), name='BookedRoom'),
     path('Room/Update/<int:bed_id>', UpdateRoom.as_view(), name='UpdateRoom'),
     path('Room/Add/<int:bed_id>', AddRoom.as_view(), name='AddRoom'),
+
     path('Calls/Current/', GetCurrentCalls.as_view(), name='CurrentCalls'),
     path('Calls/<int:Call_id>', GetCalls.as_view(), name='Calls'),
     path('Calls/GetAll/', GetAllCalls.as_view(), name='AllCalls'),
     path('Calls/Create/', CreateCalls.as_view(), name='CreateCalls'),
     path('Calls/Update/<int:Call_id>', UpdateCall.as_view(), name='UpdateCall'),
+    path('Call/History/',GetCallsHistory.as_view(), name='UserDetails'),
+
 ]
