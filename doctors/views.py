17546898a9eff4dcfcb39a84e53b081941ba1452
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-06-23 19:18:10 +0300

    edit requst

--- doctors/views.py
+++ doctors/views.py
@@ -129,10 +129,10 @@ class ReserveClinicView(APIView):
         serializer = PreserveClinicSerializer2(data=request.data)
         if serializer.is_valid():
             
-            number_in_qeue = Reservation.objects.filter(clinic =request.user.clinic, working_hour=serializer.data['working_hour_id']).count() + 1
-            if Reservation.objects.filter(clinic=request.user.clinic, working_hour=serializer.data['working_hour_id'], patient=request.user.pk).exists():
+            number_in_qeue = Reservation.objects.filter(clinic = serializer.data['clinic_id'], working_hour=serializer.data['working_hour_id']).count() + 1
+            if Reservation.objects.filter(clinic=serializer.data['clinic_id'], working_hour=serializer.data['working_hour_id'], patient=request.user.pk).exists():
                 raise ValidationError({"clinic_id": "This clinic is already reserved"})
-            clinic = Clinic.objects.get(clinic =request.user.clinic )
+            clinic = Clinic.objects.get(id = serializer.data['clinic_id'])
             user=User.objects.get(id=serializer.data.get('patient'))
             reservation = Reservation(
                 type=serializer.data.get('type'),
